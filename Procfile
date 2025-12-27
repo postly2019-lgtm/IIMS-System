92e578682f094fc27e7f96482f41091898e76302
FASIL702 <postly2019@example.com>
2025-12-27 12:01:40 +0300

    fix: Update Procfile to run startup.sh and add logging to startup script

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi --log-file - --bind 0.0.0.0:$PORT
+web: bash startup.sh
--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Starting deployment script..."
+echo "Running migrations..."
 python manage.py migrate
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
-gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 config.wsgi
+echo "Starting Gunicorn on port ${PORT:-8000}..."
+gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 --access-logfile - --error-logfile - config.wsgi
